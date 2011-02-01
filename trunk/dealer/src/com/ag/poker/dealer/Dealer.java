b00d11c01185aa42c7fa3a5ad7c087a4515f64a7
arildgreni <arildgreni@65fd3005-4800-56a5-5234-5e6ff856481b>
2011-02-01 23:34:51 +0000

    Implemented a proof of concept version of the server network code which is able to connect to clients and send a simple message.

--- trunk/dealer/src/com/ag/poker/dealer/Dealer.java
+++ trunk/dealer/src/com/ag/poker/dealer/Dealer.java
@@ -1,13 +1,36 @@
 package com.ag.poker.dealer;
 
+import com.ag.poker.dealer.network.DealerConnection;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.Toast;
 
 public class Dealer extends Activity {
+	
+	private DealerConnection mDealerConnection;
+	private Handler handler;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        handler = new Handler() {
+        	@Override
+        	public void handleMessage(Message msg) {
+        		if(msg.what == DealerConnection.SERVER_RESPONSE) {
+        			if(msg.obj instanceof String) {
+        				Toast.makeText(getApplicationContext(), msg.obj.toString(), Toast.LENGTH_LONG).show();
+        			}
+        		} 
+        	}
+        };
+        
+        this.mDealerConnection = new DealerConnection(handler);
+        
     }
 }
--- trunk/dealer/src/com/ag/poker/dealer/network/DealerConnection.java
+++ trunk/dealer/src/com/ag/poker/dealer/network/DealerConnection.java
@@ -1,47 +1,172 @@
 package com.ag.poker.dealer.network;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+import org.anddev.andengine.extension.multiplayer.protocol.adt.message.client.BaseClientMessage;
 import org.anddev.andengine.extension.multiplayer.protocol.adt.message.server.BaseServerMessage;
-import org.anddev.andengine.extension.multiplayer.protocol.client.BaseServerConnectionListener;
-import org.anddev.andengine.extension.multiplayer.protocol.client.ServerConnector;
+import org.anddev.andengine.extension.multiplayer.protocol.server.BaseClientConnectionListener;
+import org.anddev.andengine.extension.multiplayer.protocol.server.BaseClientMessageSwitch;
 import org.anddev.andengine.extension.multiplayer.protocol.server.BaseServer;
 import org.anddev.andengine.extension.multiplayer.protocol.server.ClientConnector;
+import org.anddev.andengine.extension.multiplayer.protocol.server.ClientMessageExtractor;
+import org.anddev.andengine.extension.multiplayer.protocol.server.BaseServer.IServerStateListener;
 import org.anddev.andengine.extension.multiplayer.protocol.shared.BaseConnector;
 
 import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+import com.ag.poker.dealer.utils.Constants;
 
 public class DealerConnection {
+
+	public static final int SERVER_RESPONSE = 1;
+	public static final int CLIENT_CONNECTED = 2;
 	
+	protected static final short FLAG_MESSAGE_SERVER_DEAL_CARDS = 1;
+
+	private static final int SERVER_PORT = 4444;
+
 	private BaseServer<ClientConnector> mServer;
-	private ServerConnector mServerConnector;
-	private String mServerIP = "127.0.0.1";
-	
+
 	private Handler mHandler;
-	
+
 	public DealerConnection(Handler handler) {
 		this.mHandler = handler;
-	}
-	
-	private void initServerAndClient() {
 		initServer();
-		
 	}
 
 	private void initServer() {
-		
+		this.mServer = new BaseServer<ClientConnector>(SERVER_PORT, new ClientConnectionListener(),
+				new ServerStateListener()) {
+			@Override
+			protected ClientConnector newClientConnector(final Socket pClientSocket,
+					final BaseClientConnectionListener pClientConnectionListener) throws Exception {
+				return new ClientConnector(pClientSocket, pClientConnectionListener,
+						new ClientMessageExtractor() {
+							@Override
+							public BaseClientMessage readMessage(final short pFlag,
+									final DataInputStream pDataInputStream) throws IOException {
+								return super.readMessage(pFlag, pDataInputStream);
+							}
+						}, new BaseClientMessageSwitch() {
+							@Override
+							public void doSwitch(final ClientConnector pClientConnector,
+									final BaseClientMessage pClientMessage) throws IOException {
+								super.doSwitch(pClientConnector, pClientMessage);
+								Log.i(Constants.TAG, "SERVER: ClientMessage received: "
+										+ pClientMessage.toString());
+								try {
+									DealerConnection.this.mServer.sendBroadcastServerMessage(new DealCardsServerMessage(5, 10));
+								} catch (IOException e) {
+									Log.e(Constants.TAG, "Error while sending server message", e);
+								}
+							}
+						});
+			}
+			
+		};
+
+		this.mServer.start();
+	}
+
+	private class ServerStateListener implements IServerStateListener {
+		@Override
+		public void onException(Throwable pThrowable) {
+			Message msg = DealerConnection.this.mHandler.obtainMessage(SERVER_RESPONSE);
+			msg.obj = "SERVER: Exception: " + pThrowable;
+			DealerConnection.this.mHandler.sendMessage(msg);
+			Log.i(Constants.TAG, msg.obj.toString());
+		}
+
+		@Override
+		public void onStarted(int pPort) {
+			Message msg = DealerConnection.this.mHandler.obtainMessage(SERVER_RESPONSE);
+			msg.obj = "SERVER: started at port: " + pPort;
+			DealerConnection.this.mHandler.sendMessage(msg);
+			Log.i(Constants.TAG, msg.obj.toString());
+		}
+
+		@Override
+		public void onTerminated(int pPort) {
+			Message msg = DealerConnection.this.mHandler.obtainMessage(SERVER_RESPONSE);
+			msg.obj = "SERVER: terminated at port: " + pPort;
+			DealerConnection.this.mHandler.sendMessage(msg);
+			Log.i(Constants.TAG, msg.obj.toString());
+		}
+
+	}
+
+	private class ClientConnectionListener extends BaseClientConnectionListener {
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @seeorg.anddev.andengine.extension.multiplayer.protocol.shared.
+		 * BaseConnectionListener
+		 * #onConnectInner(org.anddev.andengine.extension.multiplayer
+		 * .protocol.shared.BaseConnector)
+		 */
+		@Override
+		protected void onConnectInner(final BaseConnector<BaseClientMessage> pConnector) {
+			Message msg = DealerConnection.this.mHandler.obtainMessage(SERVER_RESPONSE);
+			msg.obj = "SERVER: Client connected: "
+					+ pConnector.getSocket().getInetAddress().getHostAddress();
+			DealerConnection.this.mHandler.sendMessage(msg);
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @seeorg.anddev.andengine.extension.multiplayer.protocol.shared.
+		 * BaseConnectionListener
+		 * #onDisconnectInner(org.anddev.andengine.extension
+		 * .multiplayer.protocol.shared.BaseConnector)
+		 */
+		@Override
+		protected void onDisconnectInner(BaseConnector<BaseClientMessage> pConnector) {
+			Message msg = DealerConnection.this.mHandler.obtainMessage(SERVER_RESPONSE);
+			msg.obj = "SERVER: Client disconnected: "
+					+ pConnector.getSocket().getInetAddress().getHostAddress();
+			DealerConnection.this.mHandler.sendMessage(msg);
+		}
+
 	}
 	
-	private class ServerConnectionListener extends BaseServerConnectionListener {
+	private static class DealCardsServerMessage extends BaseServerMessage {
+
+		public final int mCard1;
+		public final int mCard2;
+		
+		public DealCardsServerMessage(final int pCard1, final int pCard2) {
+			this.mCard1 = pCard1;
+			this.mCard2 = pCard2;
+		}
+		
+		public DealCardsServerMessage(final DataInputStream pDataInputStream) throws IOException {
+			this.mCard1 = pDataInputStream.readInt();
+			this.mCard2 = pDataInputStream.readInt();
+		}
+		
 		@Override
-		protected void onConnectInner(
-				BaseConnector<BaseServerMessage> pConnector) {
+		public short getFlag() {
+			return FLAG_MESSAGE_SERVER_DEAL_CARDS;
 		}
 
 		@Override
-		protected void onDisconnectInner(
-				BaseConnector<BaseServerMessage> pConnector) {
-			// TODO Auto-generated method stub
+		protected void onAppendTransmissionDataForToString(StringBuilder pStringBuilder) {
 			
 		}
-	}
 
+		@Override
+		protected void onWriteTransmissionData(DataOutputStream pDataOutputStream)
+				throws IOException {
+			pDataOutputStream.writeInt(this.mCard1);
+			pDataOutputStream.writeInt(this.mCard2);
+		}
+		
+	}
 }
